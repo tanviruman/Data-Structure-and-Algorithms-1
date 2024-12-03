3b643854586e4d432ea32e876a79b193998efa75
tanviruman <ruman777ahmed@gmail.com>
2024-12-03 21:54:39 +0600

    Fix Bubble Sort implementation and correct function call in main

--- DSA CT 1/BubbleSort.cpp
+++ DSA CT 1/BubbleSort.cpp
@@ -1,1 +1,52 @@
-#includestdio.h>
+#include <iostream>
+using namespace std;
+
+void Bubblesort(int arr,int n){
+
+
+
+for(int i=0;i<n-1;i++){
+
+    for (int j=0;j<n-1-i;j++)
+    {
+        int flag=0;
+        if(arr[j]>arr[j+1]){
+
+            int temp =arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]= temp;
+            flag=1;
+
+        }
+if(flag==0)
+    break;
+
+
+
+    }
+
+}
+
+
+
+
+}
+
+
+
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubble_sort(arr, n);
+    return 0;
+
+}
