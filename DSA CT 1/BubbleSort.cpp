cbde3dd1d4e030c4cbc80916190a5e5e32916702
tanviruman <ruman777ahmed@gmail.com>
2024-12-03 22:00:23 +0600

    Refactor Bubble Sort implementation for clarity and efficiency

--- DSA CT 1/BubbleSort.cpp
+++ DSA CT 1/BubbleSort.cpp
@@ -1,38 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void Bubblesort(int arr,int n){
+void bubble_sort(int arr[], int n) {
 
 
 
 for(int i=0;i<n-1;i++){
-
+    bool swapped = false;
     for (int j=0;j<n-1-i;j++)
     {
-        int flag=0;
-        if(arr[j]>arr[j+1]){
-
-            int temp =arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]= temp;
-            flag=1;
-
+        if (arr[j] > arr[j + 1]) {
+            swap(arr[j], arr[j + 1]);
+            swapped = true;
         }
-if(flag==0)
-    break;
-
-
-
     }
-
+    if (!swapped) break;
 }
 
 
 
 
 }
-
-
 
 
 int main()
@@ -43,10 +31,18 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
+    cout << "After Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
-
+    cout << endl;
     bubble_sort(arr, n);
+    cout << "After Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
-
+    }
 }
